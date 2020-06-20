e8f414d45738f11810893f8511d5362277368a60
Karan V <karanvknarayanan@gmail.com>
2020-06-20 22:13:50 +0530

    some basic code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import User from "./containers/Users";
+
+import AsyncComponent from "./hoc/AsyncComponent/AsyncComponent";
+
+const pizza = AsyncComponent(() => import("./containers/Pizza"));
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/">Users</Link> |<Link to="/pizza">Pizza</Link>
+        </div>
+        <div>
+          <Route path="/" exact component={User} />
+          <Route path="/pizza" component={Pizza} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PizzaImage from "../components/PizzaImage/PizzaImage";
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p> Awesome Users</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
